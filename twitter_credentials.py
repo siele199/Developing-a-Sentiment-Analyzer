4b89ef48fde8ec7704fca19f3360b9c8ee439b89
siele199 <60309257+siele199@users.noreply.github.com>
2022-03-27 14:53:10 +0300

    Add files via upload

---
+++ twitter_credentials.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 2 16:17:21 2022
+
+@author: vance
+"""
+#user credentials
+ACCESS_TOKEN ="1507601278280548359-ELOHIW1xCtTUMEgrRHASai8PSuB4th"
+ACCESS_TOKEN_SECRET ="myjabfLlFBKd24S3CmqJO4BXks6dtz0XgGXYVBdEvNtvw"
+CONSUMER_KEY ="MmstOcB4UqX88KYJwkWfTR16l"
+CONSUMER_SECRET ="o7q7txGnj3tp61tc6JwZNLTqJxek5JRy3A9aXK32yfvrDrS8B9"
